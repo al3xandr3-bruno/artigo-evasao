09b243db2a2f031594496eb2e503aa273187c993
Lucas Gurgel <81498538+GurgelAmaral@users.noreply.github.com>
2025-09-09 12:27:36 -0300

    Delete modelo/modelo_evasao directory

--- modelo/modelo_evasao/load_data/getFeatures.py
+++
@@ -1,8 +0,0 @@
-#captura os tipos das colunas
-
-
-def get_num_cols(df_x):
-    return df_x.select_dtypes(include='number').columns.tolist()
-
-def get_cat_cols(df_x):
-    return df_x.select_dtypes(include='object').columns.tolist()
